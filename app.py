4bdea91e0601d2178cd0f1a2262f40fcbc56a633
Marcelo Felix dos Santos <marcelosantosguaruja_noreply@users.noreply.github.com>
2026-08-30 00:37:37 -0300

    feat: Segunda versão do (app.py e README)

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
-# Programa de cálculo de medias de notas
+# Programa de cálculo de médias de notas
 # Autor: Marcelo Félix dos antos
 
 # Entrada
 nome   = input("Digite o nome do aluno: ")
-nota_1 = float(input("Digite a primeira nota: "))
-nota_2 = float(input("Digite a segunda nota: "))
+nota_1 = float(input("Digite a nota do primeiro bimestre: "))
+nota_2 = float(input("Digite a nota do segundo bimestre: "))
+nota_3 = float(input("Digite a nota do terceiro bimestre: "))
+nota_4 = float(input("Digite a nota do quarto bimestre: "))
 
 # Processamento
-media = (nota_1 + nota_2) / 2
+media = (nota_1 + nota_2 + nota_3 + nota_4) / 4
 
 # Saída
-print(f"\n Aluno: {nome}")
-print(f"\n Média: {media:.2f}")
+print(f"\n Aluno      : {nome}")
+print(f"\n Média Final: {media:.2f}")
 
 if media >= 6:
-    print("Situação: APROVADO")
+    print("Situação: Parabéns o aluno foi APROVADO")
 else:
-    print("Situação: REPROVADO")
+    print("Situação: Não foi desta vez. Aluno REPROVADO")
